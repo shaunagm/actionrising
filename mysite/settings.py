54395d1612b393958328dcb62430cf22ef677821
Trey Hunner <trey@treyhunner.com>
2016-11-17 22:52:48 -0800

    Add django-activity-stream for Django 1.10

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'act-now.herokuapp.com']
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -36,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'actstream',
     'mysite',
     'profiles',
     'actions',
