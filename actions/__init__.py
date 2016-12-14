e78b646309cc948df3f08d6f16dc55eeb32e95c2
Goss Nuzzo-Jones <gossnj@gmail.com>
2016-12-13 21:57:48 -0500

    fixes for rebase

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,11 +1,1 @@
-# -*- coding: utf-8 -*-
-from django.apps import AppConfig
-
-class ActionsConfig(AppConfig):
-    """Setup our app."""
-    name = 'actions'
-    
-    def ready(self):
-        import actions.signals 
-        
-default_app_config = 'actions.ActionsConfig'  
+default_app_config = 'actions.apps.ActionsConfig'
