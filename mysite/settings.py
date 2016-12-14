f682edca1d27d9ec1c4a7be0d90b82648a676509
shaunagm <shaunagm@gmail.com>
2016-12-14 17:35:22 -0500

    Change secret key and move to env variable

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p_3=uyh6@0ngi)d8tl+idm-7682qb0c4b)15cj06^99em3q$&x'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
