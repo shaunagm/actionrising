af33356759cb81471bdda980e28d9f1762c66bd8
shaunagm <shaunagm@gmail.com>
2016-12-18 21:04:39 -0500

    Redirect to password change after login on first login

--- mysite/views.py
+++ mysite/views.py
@@ -14,7 +14,7 @@ def about(request):
     return render(request, 'mysite/about.html', {'time_elapsed': time_elapsed})
 
 def change_password_redirect(request):
-    return HttpResponseRedirect(reverse('profile', kwargs={'pk': request.user.pk }))
+    return render(request, 'mysite/landing.html')
 
 def acme_challenge(request):
     return render(request, 'mysite/challenge.html')
