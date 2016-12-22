71bad6e3407f866c540da56de83ac1c42bd40c09
shaunagm <shaunagm@gmail.com>
2016-12-22 18:53:34 -0500

    Fix bug with as_view()

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,5 +4,5 @@ from notifications import views
 
 urlpatterns = [
     url(r'^nonuseremail/$',  views.nonuser_notification, name='nonuser_notifications'),
-    url(r'^(?P<pk>[0-9]+)/$',  views.SettingsEditView.as_view, name='manage_notifications'),
+    url(r'^(?P<pk>[0-9]+)/$',  views.SettingsEditView.as_view(), name='manage_notifications'),
 ]
