cc4a16960d85fb273656ed8a45c5ffd5c18799c4
Presley Pizzo <presleyanne@gmail.com>
2017-04-15 16:14:11 -0400

    Remove NavbarSettings.
    I think we should guide users towards a good way of using the site
    without needing to provide too many options.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
 from profiles.models import (Profile, Relationship, ProfileActionRelationship,
-    ProfileSlateRelationship, PrivacyDefaults, NavbarSettings)
+    ProfileSlateRelationship, PrivacyDefaults)
 
 admin.site.register(Profile)
 admin.site.register(Relationship)
 admin.site.register(ProfileActionRelationship)
 admin.site.register(ProfileSlateRelationship)
 admin.site.register(PrivacyDefaults)
-admin.site.register(NavbarSettings)
