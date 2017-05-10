606240671f7f9f91b074c1bafc266aff08597a07
praccu <praccu@gmail.com>
2017-05-10 00:45:50 -0400

    Initial check-in of constants.py which stores shared constants.
    This is the first check in of a series of commits to pull out shared
    constants, which create unnecessary brittleness e.g. if a constant is
    referenced both in code and a template.

---
+++ constants.py
@@ -0,0 +1,10 @@
+"""Store constant values that are shared across the code base.
+
+This module stores constants which are referenced from multiple places. If
+the constant is only used locally, please define it locally for clarity.
+
+Constant names should be descriptive and in ALL CAPS.
+
+Todo:
+	* Migrate the constants!
+"""
