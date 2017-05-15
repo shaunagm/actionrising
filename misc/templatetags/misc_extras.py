00c10aa54c177699fd2f74979221ceeef144c427
Alex Riina <alex.riina@gmail.com>
2017-05-14 23:01:15 -0400

    strap non-display tags from richtextfields

--- misc/templatetags/misc_extras.py
+++ misc/templatetags/misc_extras.py
@@ -1,12 +1,23 @@
 import random
+from bleach import clean
 from django import template
-from django.template.loader import select_template
-from plugins.site_plugin_conf import plugins
+from django.utils.safestring import mark_safe, SafeData
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 quote_list = ["'How wonderful it is that nobody need wait a single moment before starting to improve the world.' - Anne Frank"]
 
+
 @register.simple_tag()
 def get_footer_quote():
     return random.choice(quote_list)
+
+
+@register.filter
+@stringfilter
+def bleach(value):
+    if isinstance(value, SafeData):
+        return value
+    else:
+        return mark_safe(clean(value))
