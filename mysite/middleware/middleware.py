fc526cd7371ea1151002d20e36dfb24903e5bf00
shaunagm <shaunagm@gmail.com>
2017-05-15 22:41:32 -0400

    Refactor middleware into helper functions

--- mysite/middleware/middleware.py
+++ mysite/middleware/middleware.py
@@ -7,40 +7,41 @@ from django.utils.deprecation import MiddlewareMixin
 
 from plugins.location_plugin.models import Location
 
+DEFAULT_TIMEZONE = 'America/New_York'
+
 class TimezoneMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-
-        # Skip for anonymous users
+        '''Middleware gets timezone for all views, updates when referred to by
+        /profiles/edit, the only place where location can be updated'''
         if not request.user.is_authenticated:
-            timezone.activate(pytz.timezone('America/New_York'))
+            timezone.activate(pytz.timezone(DEFAULT_TIMEZONE))
             return
 
-        # If user is coming from edit profile, where they may have edited location,
-        # try to calculate timezone
         if "/profiles/edit" in request.META.get('HTTP_REFERER', ''):
-            loc = self.get_location(request.user)
-            if loc:
-                tz = self.get_timezone(loc)
-                if tz:
-                    request.session['django_timezone'] = tz
-            else:
-                request.session['django_timezone'] = None
+            request.session['django_timezone'] = self.update_timezone(request)
 
-        # If user has timezone calculated, apply
-        tzname = request.session.get('django_timezone')
-        if tzname:
-            timezone.activate(pytz.timezone(tzname))
+        self.activate_timezone(request.session.get('django_timezone'))
+
+    def update_timezone(self, request):
+        '''Update timezone if location & timezone exists, or if location has been
+        removed, otherwise return current timezone'''
+        loc = self.get_location(request.user)
+        if loc:
+            tz = self.get_timezone(loc)
+            return tz if tz else request.session.get('django_timezone')
         else:
-            timezone.activate(pytz.timezone('America/New_York'))
+            return None
 
     def get_location(self, user):
+        '''Get location with lat/lon specified'''
         ctype = ContentType.objects.get_for_model(user.profile)
         location = Location.objects.filter(content_type=ctype, object_id=user.profile.pk).first()
         if location and location.lat and location.lon:
             return location
 
     def get_timezone(self, location):
+        '''Get timezone given location'''
         try:
             geocoder = GoogleV3()
             tz = geocoder.timezone([location.lat, location.lon])
@@ -48,3 +49,10 @@ class TimezoneMiddleware(MiddlewareMixin):
                 return tz.zone
         except:
             traceback.print_exc()
+
+    def activate_timezone(self, tzname):
+        '''If user has timezone set, apply'''
+        if tzname:
+            timezone.activate(pytz.timezone(tzname))
+        else:
+            timezone.activate(pytz.timezone(DEFAULT_TIMEZONE))
