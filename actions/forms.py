567b80131def3395a948e41282b72b38900f48c2
shaunagm <shaunagm@gmail.com>
2017-05-19 14:02:54 -0400

    Limit deadlines to the future

--- actions/forms.py
+++ actions/forms.py
@@ -1,3 +1,4 @@
+import datetime
 from django import forms
 from datetimewidget.widgets import DateTimeWidget
 from django.forms.widgets import HiddenInput
@@ -17,8 +18,8 @@ class SlateChoiceField(forms.ModelMultipleChoiceField):
 class ActionForm(forms.ModelForm):
     slates = SlateChoiceField(queryset=Slate.objects.all(), label="Add to your slates", required=False)
     deadline = forms.DateTimeField(required=False, input_formats=['%m/%d/%Y %I%p'],
-        widget=DateTimeWidget(options={'format': 'mm/dd/yyyy HP', 'minView': '1'},
-        bootstrap_version=3), help_text='Day/Month/Year Hour')
+        widget=DateTimeWidget(options={'format': 'mm/dd/yyyy HP', 'minView': '1',
+        'startDate': str(datetime.date.today())}, bootstrap_version=3), help_text='Day/Month/Year Hour')
 
     class Meta:
         model = Action
