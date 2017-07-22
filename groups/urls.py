b00f3c092e0edb5c75ce8e5d2585343952d387c5
Shauna Gordon-McKeon <shaunagm@gmail.com>
2017-07-22 18:15:55 -0400

    Create shells of group views
    Includes corresponding templates & lines in urls.py

---
+++ groups/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from groups import views
+
+urlpatterns = [
+    url(r'^groups/$', views.GroupListView.as_view(), name='groups'),
+    url(r'^group/(?P<slug>[-\w]+)/$', views.GroupView.as_view(), name='group'),
+    url(r'^edit/(?P<slug>[-\w]+)/$', views.GroupEditView.as_view(), name='edit_group'),
+    url(r'^admin/(?P<slug>[-\w]+)/$', views.GroupAdminView.as_view(), name='admin_group'),
+    url(r'^create/$', views.GroupCreateView.as_view(), name='create_group'),
+]
--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import UserPassesTestMixin,  LoginRequiredMixin
 
-# Create your views here.
+from groups.models import GroupProfile
+
+
+class GroupListView(generic.ListView):
+    template_name = "groups/groups.html"
+    model = GroupProfile
+
+
+class GroupView(UserPassesTestMixin, generic.DetailView):
+    template_name = "groups/group.html"
+    model = GroupProfile
+    slug_field = "groupname"
+
+
+class GroupCreateView(LoginRequiredMixin, generic.edit.CreateView):
+    model = GroupProfile
+    template_name = "groups/create_group.html"
+
+
+class GroupEditView(UserPassesTestMixin, generic.edit.UpdateView):
+    model = GroupProfile
+    slug_field = "groupname"
+    template_name = "groups/edit_group.html"
+
+
+class GroupAdminView(UserPassesTestMixin, generic.DetailView):
+    emplate_name = "groups/admin_group.html"
+    model = GroupProfile
+    slug_field = "groupname"
