b46572b6602a09be8c67e1ef9b3718f0ba5f0ec9
Shauna Gordon-McKeon <shaunagm@gmail.com>
2017-07-26 17:49:29 -0400

    Add groups unit tests for step 2

--- groups/test_groups.py
+++ groups/test_groups.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
-from django.utils import timezone
+from django.contrib.auth.models import AnonymousUser
+from django.forms.widgets import HiddenInput
 
 from groups.models import GroupProfile
+from mysite.lib.choices import PrivacyChoices
+from groups.forms import GroupForm
 
 from groups import factories as group_factories
 from profiles import factories as profile_factories
@@ -16,6 +19,8 @@ class TestGroupProfileMethods(TestCase):
         self.groupprofile = group_factories.GroupProfile()
         self.test_member = profile_factories.Profile()
         self.test_admin = profile_factories.Profile()
+        self.test_nonmember = profile_factories.Profile()
+        self.test_anon_user = AnonymousUser()
 
     def test_hasmember(self):
         self.assertFalse(self.groupprofile.hasMember(self.test_member.user))
@@ -70,6 +75,11 @@ class TestGroupProfileMethods(TestCase):
             owner=self.test_member.user)
         self.assertEqual(test_group.group.name, "a-test-groupname")
 
+    def test_add_owner_as_admin_on_save(self):
+        test_group = GroupProfile.objects.create(groupname="A test groupname",
+                                                 owner=self.test_member.user)
+        self.assertTrue(test_group.hasAdmin(self.test_member.user))
+
     def test_get_urls(self):
         test_group = GroupProfile.objects.create(groupname="A test groupname",
                                                  owner=self.test_member.user)
@@ -77,3 +87,49 @@ class TestGroupProfileMethods(TestCase):
         self.assertEqual(test_group.get_edit_url(), "/groups/edit/a-test-groupname/")
         self.assertEqual(test_group.get_admin_url(), "/groups/admin/a-test-groupname/")
 
+    def test_is_visible_when_public(self):
+        '''Should appear to all users: member, non-member and non-user'''
+        self.groupprofile.privacy = PrivacyChoices.public
+        self.groupprofile.save()
+        self.assertTrue(self.groupprofile.is_visible_to(self.test_anon_user))
+        self.assertTrue(self.groupprofile.is_visible_to(self.test_nonmember.user))
+        self.groupprofile.addMember(self.test_member.user)
+        self.assertTrue(self.groupprofile.is_visible_to(self.test_member.user))
+
+    def test_is_visible_when_sitewide(self):
+        '''Should appear only to member and non-member, not non-user'''
+        self.groupprofile.privacy = PrivacyChoices.sitewide
+        self.groupprofile.save()
+        self.assertFalse(self.groupprofile.is_visible_to(self.test_anon_user))
+        self.assertTrue(self.groupprofile.is_visible_to(self.test_nonmember.user))
+        self.groupprofile.addMember(self.test_member.user)
+        self.assertTrue(self.groupprofile.is_visible_to(self.test_member.user))
+
+    def test_is_visible_when_members_only(self):
+        '''Should appear only to member'''
+        self.groupprofile.privacy = PrivacyChoices.members
+        self.groupprofile.save()
+        self.assertFalse(self.groupprofile.is_visible_to(self.test_anon_user))
+        self.assertFalse(self.groupprofile.is_visible_to(self.test_nonmember.user))
+        self.groupprofile.addMember(self.test_member.user)
+        self.assertTrue(self.groupprofile.is_visible_to(self.test_member.user))
+
+    def test_get_members(self):
+        self.groupprofile.addMember(self.test_member.user)
+        self.assertEquals(list(self.groupprofile.get_members()),
+            [self.groupprofile.owner, self.test_member.user])
+
+
+class TestGroupForms(TestCase):
+
+    def setUp(self):
+        self.test_owner = profile_factories.Profile()
+        self.groupprofile = group_factories.GroupProfile()
+
+    def test_user_set_on_init(self):
+        initial_form = GroupForm(user=self.test_owner)
+        self.assertEquals(initial_form.user, self.test_owner)
+
+    def test_groupname_hidden_on_update(self):
+        update_form = GroupForm(instance=self.groupprofile)
+        self.assertEqual(type(update_form.fields['groupname'].widget), HiddenInput)
